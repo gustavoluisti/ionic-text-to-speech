c96920b3418401782d96cf9521b729469e603e33
Luis Gustavo Santos <luisgustavosantos@spartas-MacBook-Pro.local>
2019-01-05 22:30:16 -0200

    Testando recurso

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  textoSerFalado: string = 'Olá Mundo';
 
-  constructor(public navCtrl: NavController) {
+  constructor(private tts: TextToSpeech) {
 
+  }
+
+  falar() {
+    this.tts.speak({
+      text: this.textoSerFalado,
+      locale: 'pt-BR',
+      rate: 0.75})
+      .then(() => alert('Falou com sucesso!'))
+      .catch((reason: any) => alert(reason))
   }
 
 }
